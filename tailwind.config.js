a51bcf907fcca5043566c8e00d4918a8d56f47f2
Ajibade Gabriel Fikayo <gabrielajibade4@gmail.com>
2024-07-18 23:53:11 +0100

    final try

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens: {
+        widescreen: {
+          row: "(min-aspect-ration: 3/2)",
+        },
+        tailscreen: {
+          row: "(max-aspect-ration: 13/20)",
+        },
+      },
+      keyframes: {
+        "open-menu": {
+          "0%": { transform: "scaleY(0)" },
+          "80%": { transform: "scaleY(1.2)" },
+          "100%": { transform: "scaleY(1)" },
+        },
+      },
+      animation: {
+        "open-menu": "open-menu 0.5s ease-in-out forwards",
+      },
+    },
+  },
+  plugins: [],
+};
